caa1f46e5475ce80b6fed8688d8a5362e7179320
zhoucheng <zhoucheng133@outlook.com>
2025-07-01 19:42:46 +0800

    自定义标题栏

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,3 +1,15 @@
 <template>
-  <div>Hello World!!!</div>
+  <div class="dragArea"></div>
+  <div style="margin-top: 30px;">Hello World!!!</div>
 </template>
+
+<style scoped>
+.dragArea{
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 30px;
+  width: 100%;
+  -webkit-app-region: drag;
+}
+</style>
